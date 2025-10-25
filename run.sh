a73d1c0e58e4ea2016c15b68fb0816587a35dec8
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-10-26 03:07:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -72,7 +72,7 @@ print_green "Configuration files have been checked."
 
 print_yellow "Checking dependencies..."
 cd "$MODULES_DIR"
-npm install user-agents axios meo-forkcy-colors meo-forkcy-utils https-proxy-agent socks-proxy-agent
+npm install user-agents axios meo-forkcy-colors meo-forkcy-utils meo-forkcy-logger meo-forkcy-proxy
 cd - > /dev/null
 print_green "Dependencies installation completed!"
 
